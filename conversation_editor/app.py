2732e598a1dfdaf2c561b038d42c60c30dd0d7d8
Ramon Meza <ramon_meza@live.com>
2022-09-18 03:37:59 -0400

    Downgrade DPG due to menu_bar bug.

--- conversation_editor/app.py
+++ conversation_editor/app.py
@@ -7,13 +7,26 @@ PRIMARY_WINDOW_TAG: str = 'Primary Window'
 def main() -> None:
     dpg.create_context()
 
+    dpg.create_viewport(title='Custom Title', width=800, height=600)
+
     with dpg.window(tag=PRIMARY_WINDOW_TAG):
-        dpg.add_text('Hello, world!')
+        with dpg.menu_bar():
+            with dpg.menu(label='File'):
+                dpg.add_menu_item(label='New')
+                dpg.add_separator()
+                dpg.add_menu_item(label='Open')
+                dpg.add_separator()
+                dpg.add_menu_item(label='Save')
+                dpg.add_menu_item(label='Save As')
+                dpg.add_separator()
+                dpg.add_menu_item(label='Exit')
+
+        with dpg.node_editor():
+            pass
 
     dpg.setup_dearpygui()
-    dpg.create_viewport(title='Conversation Editor', width=800, height=600)
-    dpg.set_primary_window(PRIMARY_WINDOW_TAG, True)
     dpg.show_viewport()
+    dpg.set_primary_window(PRIMARY_WINDOW_TAG, True)
     dpg.start_dearpygui()
     dpg.destroy_context()
 
